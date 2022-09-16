8da23e1aadd1027dd26167be0a17f04b0fb5b5af
Meenu Sowjanya Chandrasekaran <mchandrasekaran@fwsa.freshworks.com>
2022-09-16 12:03:46 +0530

    Reverse Array - Easy

---
+++ reverseArray (Easy).rb
@@ -0,0 +1,14 @@
+def reverseArray(a)
+    # Write your code here
+    # a.reverse()
+    temp = a
+    tmp = []
+    for i in 0..(temp.length)
+        pop = a.pop
+        tmp << pop
+    end
+    a = tmp
+    return a
+end
+
+puts reverseArray([1,3,6,9])
