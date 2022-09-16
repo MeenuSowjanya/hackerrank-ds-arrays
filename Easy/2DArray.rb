ae2ea7c48b1fc9b51f0e5af6edf92ef39c85f32c
Meenu Sowjanya Chandrasekaran <mchandrasekaran@fwsa.freshworks.com>
2022-09-16 12:32:23 +0530

    Added meaningful names

--- Easy/2DArray.rb
+++ Easy/2DArray.rb
@@ -1,19 +1,23 @@
 #There are 16  hourglasses in arr . An hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum. The array will always be6 X 6.
-def hourglassSum(arr)
+def hourglassSum(hourglass_array)
     # Write your code here
-    temp = []
+    temporary_array = []
     # Row Loop
-    4.times do |i|
+    4.times do |row|
         # Column Loop
-        4.times do |j|
-             temp << arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+        4.times do |column|
+             temporary_array << calculate_sum_of_hourglassSet(hourglass_array,row,column)
         end
     end
     # Returns Maximum Hourglass Sum
-    temp.max
+    temporary_array.max
 end
 
-params_array = [1,2,3,4,5,6]
-puts hourglassSum([params_array, params_array, params_array, params_array, params_array,params_array])
+def calculate_sum_of_hourglassSet(hourglass_array,row,column)
+    sum = hourglass_array[row][column] + hourglass_array[row][column+1] + hourglass_array[row][column+2] + hourglass_array[row+1][column+1] + hourglass_array[row+2][column] + hourglass_array[row+2][column+1] + hourglass_array[row+2][column+2]
+end
+
+hourglass_input_array_set = [1,2,3,4,5,6]
+puts hourglassSum([hourglass_input_array_set, hourglass_input_array_set, hourglass_input_array_set, hourglass_input_array_set, hourglass_input_array_set,hourglass_input_array_set])
 
 
