ca3d2ea1c5ff2a2a8004c9e13de1eb3cba135ccd
Meenu Sowjanya Chandrasekaran <mchandrasekaran@fwsa.freshworks.com>
2022-09-28 11:59:27 +0530

    Review Changes * Added meaningful names * Created methods for looping

--- Easy/2DArray.rb
+++ Easy/2DArray.rb
@@ -18,7 +18,7 @@ def traversing_through_each_hourglass(count_of_each_row_of_a_hourglass_array, re
 end
 
 def calculate_sum_of_hourglassSet(hourglass_array,row_count,column_count)
-    sum = hourglass_array[row_count][column] + hourglass_array[row_count][column_count+1] + hourglass_array[row_count][column_count+2] + hourglass_array[row_count+1][column_count+1] + hourglass_array[row_count+2][column_count] + hourglass_array[row_count+2][column_count+1] + hourglass_array[row_count+2][column_count+2]
+    sum = hourglass_array[row_count][column_count] + hourglass_array[row_count][column_count+1] + hourglass_array[row_count][column_count+2] + hourglass_array[row_count+1][column_count+1] + hourglass_array[row_count+2][column_count] + hourglass_array[row_count+2][column_count+1] + hourglass_array[row_count+2][column_count+2]
 end
 
 hourglass_input_array_set = [1,2,3,4,5,6]
