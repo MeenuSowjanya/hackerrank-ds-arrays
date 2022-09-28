f6252729183ec8406374e453f17699388c506030
Meenu Sowjanya Chandrasekaran <mchandrasekaran@fwsa.freshworks.com>
2022-09-28 11:58:20 +0530

    Review Changes * Added meaningful names * Created methods for looping

--- Easy/2DArray.rb
+++ Easy/2DArray.rb
@@ -1,20 +1,24 @@
 #There are 16  hourglasses in arr . An hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum. The array will always be6 X 6.
 def hourglassSum(hourglass_array)
-    # Write your code here
-    temporary_array = []
-    # Row Loop
-    4.times do |row|
-        # Column Loop
-        4.times do |column|
-             temporary_array << calculate_sum_of_hourglassSet(hourglass_array,row,column)
-        end
-    end
-    # Returns Maximum Hourglass Sum
-    temporary_array.max
+    result_array = []
+    return traversing_through_the_input_array(hourglass_array, result_array).max
 end
 
-def calculate_sum_of_hourglassSet(hourglass_array,row,column)
-    sum = hourglass_array[row][column] + hourglass_array[row][column+1] + hourglass_array[row][column+2] + hourglass_array[row+1][column+1] + hourglass_array[row+2][column] + hourglass_array[row+2][column+1] + hourglass_array[row+2][column+2]
+def traversing_through_the_input_array(hourglass_array, result_array)
+    4.times do |count_of_each_row_of_a_hourglass_array|
+        traversing_through_each_hourglass(count_of_each_row_of_a_hourglass_array, result_array)
+    end
+    result_array
+end
+
+def traversing_through_each_hourglass(count_of_each_row_of_a_hourglass_array, result_array)
+    4.times do |count_of_each_column_of_a_hourglass_set|
+        result_array << calculate_sum_of_hourglassSet(hourglass_array,count_of_each_row_of_a_hourglass_array,count_of_each_column_of_a_hourglass_set)
+   end
+end
+
+def calculate_sum_of_hourglassSet(hourglass_array,row_count,column_count)
+    sum = hourglass_array[row_count][column] + hourglass_array[row_count][column_count+1] + hourglass_array[row_count][column_count+2] + hourglass_array[row_count+1][column_count+1] + hourglass_array[row_count+2][column_count] + hourglass_array[row_count+2][column_count+1] + hourglass_array[row_count+2][column_count+2]
 end
 
 hourglass_input_array_set = [1,2,3,4,5,6]
