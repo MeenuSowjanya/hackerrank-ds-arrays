ef066a1b2b3941f5936bd0dc0c98157183845d52
Meenu Sowjanya Chandrasekaran <mchandrasekaran@fwsa.freshworks.com>
2022-09-30 09:35:44 +0530

    Review changes 2: 1. Added comments fr code readability

--- Hard/arrayManipulation.rb
+++ Hard/arrayManipulation.rb
@@ -10,9 +10,11 @@ def array_manipulation(number_of_elements_in_the_array, input_array_named_querie
   
     maximum_value_in_result_hash_of_arrays = 0
     current_element_in_the_specific_index = 0
+
+    # Method which gives a sorted array of unique elements
     relevant_indexes.sort.uniq.each do |index|
       current_element_in_the_specific_index += result_hash[index] if result_hash.key?(index)
-      maximum_value_in_result_hash_of_arrays = [maximum_value_in_result_hash_of_arrays, current].max
+      maximum_value_in_result_hash_of_arrays = [maximum_value_in_result_hash_of_arrays, current_element_in_the_specific_index].max
     end
   
     maximum_value_in_result_hash_of_arrays
