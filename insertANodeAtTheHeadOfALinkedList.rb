61601bec20b80e0413d68c427e1f4baba18294b3
Meenu Sowjanya Chandrasekaran <mchandrasekaran@fwsa.freshworks.com>
2022-10-17 12:03:58 +0530

    Insert a node at the head of a linked list

---
+++ insertANodeAtTheHeadOfALinkedList.rb
@@ -0,0 +1,8 @@
+number_of_elements_in_the_linked_list = gets.chomp.to_i
+given_linked_list_temporary = []
+(number_of_elements_in_the_linked_list-1).times do
+   given_linked_list_temporary << gets.chomp.to_i 
+end
+given_linked_list = given_linked_list_temporary.reverse!
+given_linked_list.unshift(gets.chomp.to_i) 
+puts given_linked_list
